2af08fc7aea50622c5ed2d08cfd68557392d0432
John Stray <johnstray@users.noreply.github.com>
2022-07-16 01:27:03 +0930

    Created footer include

---
+++ themes/Yawen/includes/footer.inc.php
@@ -0,0 +1,17 @@
+<?php
+/**
+ * Yawen Theme for GetSimple CMS
+ * A theme designed to be simple yet elegant, focusing more on well constructed
+ * content than the website itself. Has support for dark-mode where possible, and
+ * tries to be as accessible as possible for all users.
+ *
+ * @Author:     John Stray
+ * @URL:        https://gs.johnstray.com/themes/yawen
+ * @Version:    v1.0.0
+ **/
+
+// This file should not be loaded directly. It should be loaded in the context of GetSimple CMS
+if ( defined('IN_GS') === false ) { echo "You cannot load this file directly!"; header("Location: /"); }
+
+?>
+        <p class="u-no-margin u-align-text--center"><small>Copyright <?php echo date('Y'); ?> &copy; <?php get_site_name(); ?> - All rights reserved.</small></p>
