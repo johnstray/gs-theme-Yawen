4a7ba043054675f5f5f0c976a81b9c75bda716dd
John Stray <johnstray@users.noreply.github.com>
2022-07-16 01:20:46 +0930

    Updates to stylesheets

--- themes/Yawen/includes/header.inc.php
+++ themes/Yawen/includes/header.inc.php
@@ -1,4 +1,4 @@
-<?php if ( defined('IN_GS') === false ) { echo "You cannot load this file directly!"; header("Location: /"); }
+<?php
 /**
  * Yawen Theme for GetSimple CMS
  * A theme designed to be simple yet elegant, focusing more on well constructed
@@ -9,21 +9,24 @@
  * @URL:        https://gs.johnstray.com/themes/yawen
  * @Version:    v1.0.0
  **/
-?><!DOCTYPE html><!-- HTML 5 : XHTML Compatible -->
-<html lang="en"><!-- @TODO: Set GS lang here -->
-    <head>
+
+// This file should not be loaded directly. It should be loaded in the context of GetSimple CMS
+if ( defined('IN_GS') === false ) { echo "You cannot load this file directly!"; header("Location: /"); }
+
+?><head>
         <meta charset="utf-8" />
         <meta http-equiv="content-type" content="text/html; charset=utf-8" />
         <meta http-equiv="X-UA-Compatible" content="IE=edge" />
         <meta name="viewport" content="width=device-width, initial-scale=1" />
-        <meta name="robots" content="index,follow" />
-
-        <title><?php get_head_title(); ?></title>
+        
+        <!-- Metadata -->
+        <title><?php get_page_title(); ?> &raquo; <?php get_site_name(); ?></title>
         <link rel="shortcut icon" href="favicon.ico" />
 
         <!-- Stylesheets -->
-        <link rel="stylesheet" id="vanilla-framework_v3.6.1" href="<?php get_theme_url(); ?>/assets/styles/vanilla-framework.css" />
-        <link rel="stylesheet" href="<?php get_theme_url(); ?>/assets/styles/yawen-styles.css" />
+        <link rel="stylesheet" id="vanilla-framework_v3.6.1" href="<?php get_theme_url(); ?>/assets/styles/vanilla-framework.min.css" />
+        <link rel="stylesheet" href="<?php get_theme_url(); ?>/assets/styles/theme-styles.css" />
 
         <?php get_header(); // GetSimple plugin hook - used by plugins to inject their code ?>
+        
     </head>
